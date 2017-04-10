f522fd706fec0826e62d98ac8b8a724c798d8135
Aixa <aixacasanova@gmail.com>
2017-04-09 23:29:52 -0300

    se modifica recursos y logica
    se modifica recursos y logica

--- Trivia/src/pages/juego/juego.ts
+++ Trivia/src/pages/juego/juego.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {BaseRequestOptions, Http} from '@angular/http';
 import { NavController } from 'ionic-angular';
 import 'rxjs/Rx';
-
+import { login } from '../login/login'
 
 
 @Component({
@@ -18,6 +18,19 @@ export class juego
     r2="rta";
     r3="rta";
     r4="rta";
+    Lapreg="";
+    btncolor1='';
+    btncolor2='';
+    btncolor3='';
+    btncolor4='';
+    ContOK=0;
+    ContNoOK=0;
+    private banderaOk: boolean = false;
+    private veo: boolean = false;
+    private veoini: boolean = true;
+    private veoFin: boolean = false;
+    resultOK="";
+    resultNoOK="";
     listaPreguntas={
         1:{
           pr:"en que paises se encuentra cordoba?",
@@ -41,43 +54,70 @@ export class juego
         } 
     };
 
-        listaRespuestas={
-        1:{r1:"Argentina y España",r2:"Colombia y Venezuela",r3:"Paraguay y Portugal",r4:"Francia y Alemania"},  
-        2:{r1:"Profesor Neurus",r2:"No tiene",r3:"Pepe",r4:"Leo"},  
-        3:{r1:"Rayuela",r2:"Generala",r3:"Ludo",r4:"T.E.G"},  
-        4:{r1:"2001",r2:"2003",r3:"2006",r4:"2007"},
-        5:{r1:"Veinticinco",r2:"Diez",r3:"Cinco",r4:"Veinte"}       
-         };
-        constructor(public navCtrl: NavController, public http: Http) 
-        {
-           
-       
-            this.verPreguntaYResp(this.contador);
-       
-        }
+    listaRespuestas={
+      1:{r1:"Argentina y España",r2:"Colombia y Venezuela",r3:"Paraguay y Portugal",r4:"Francia y Alemania"},  
+      2:{r1:"Profesor Neurus",r2:"No tiene",r3:"Pepe",r4:"Leo"},  
+      3:{r1:"Rayuela",r2:"Generala",r3:"Ludo",r4:"T.E.G"},  
+      4:{r1:"2001",r2:"2003",r3:"2006",r4:"2007"},
+      5:{r1:"Veinticinco",r2:"Diez",r3:"Cinco",r4:"Veinte"}       
+    };
     
-    verResp(preg,re)
+    constructor(public navCtrl: NavController, public http: Http) 
+    {
+      this.verPreguntaYResp(this.contador);
+    }
+    
+    verResp(pregu,re,nrobtn)
     {    
+      
       console.info("empieza con: "+this.contador);
       if(this.contador <= 5)
       {
           for (let preg in this.listaPreguntas) 
           {
-              preg = this.listaPreguntas[preg];
-              if(re == preg['re'])
-              {
-              console.info("ok: "+ preg['re']);
+              this.Lapreg = this.listaPreguntas[preg];
+              if(pregu == this.Lapreg['pr'] && re == this.Lapreg['re'])
+              { 
+                    console.info("ok: "+ this.Lapreg['re']);
+                    this.banderaOk=true;
+                  
+                    break;                
               }
-          } 
+              
+          }
+
+          if(this.banderaOk==true)
+          {
+            this.ContOK++;
+             if(nrobtn==1){this.btncolor1='green';}
+             else if(nrobtn==2){this.btncolor2='green';}
+             else if(nrobtn==3){this.btncolor3='green';}
+             else if(nrobtn==4){this.btncolor4='green';}
+          }
+          else{ 
+            this.ContNoOK++;
+            if(nrobtn==1){this.btncolor1='red';}
+            else if(nrobtn==2){this.btncolor2='red';}
+            else if(nrobtn==3){this.btncolor3='red';}
+            else if(nrobtn==4){this.btncolor4='red';}
+                
+          }
+
       
            this.contador++;
             console.info("termina con: "+this.contador);
-            if(this.contador <= 5)
-              {
-                this.verPreguntaYResp(this.contador);  
-              }  
+
+             this.veo=true; 
         } 
-        if(this.contador > 5){alert("se termino el juego");}
+
+         
+         if(this.contador > 5){
+           this.veo=false;        
+           this.veoini=false;
+           this.veoFin=true;
+           this.resultOK="Correctas: " + this.ContOK;
+           this.resultNoOK="Incorrectas: " + this.ContNoOK;
+         }
 
       }
 
@@ -88,8 +128,28 @@ export class juego
         this.r2=this.listaRespuestas[ind]['r2'];
         this.r3=this.listaRespuestas[ind]['r3'];
         this.r4=this.listaRespuestas[ind]['r4'];
+        
+        this.btncolor1='';
+        this.btncolor2='';
+        this.btncolor3='';
+        this.btncolor4='';
+        this.banderaOk = false;
+        this.veo= false;
+
+
+        if(this.contador > 5){
+
+           this.veo=false;        
+           this.veoini=false;
+           this.veoFin=true;
+           this.resultOK="Correctas: " + this.ContOK;
+           this.resultNoOK="Incorrectas: " + this.ContNoOK;
+         }
       }
  
- 
+ volverAEmpezar()
+ {
+    this.navCtrl.setRoot(login);
+ }
 
 }
