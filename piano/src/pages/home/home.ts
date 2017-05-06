2f63fdb789202207cd4738726f8068d937d8a944
Aixa <aixacasanova@gmail.com>
2017-05-06 03:31:15 -0300

    se agrega guardar en file

--- piano/src/pages/home/home.ts
+++ piano/src/pages/home/home.ts
@@ -4,7 +4,7 @@ import { Platform } from 'ionic-angular';
 import { NativeAudio } from '@ionic-native/native-audio';
 import { Vibration } from '@ionic-native/vibration';
 import { File } from '@ionic-native/file';
-
+import { LoginPage } from '../login/login';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -13,6 +13,9 @@ export class HomePage {
 
   usuarioNombre;
   archivo;
+  todo=[];
+  nota;
+  aguardar;
   constructor(private file: File,public navCtrl: NavController, plt: Platform, private nativeAudio: NativeAudio, private vibration: Vibration,params:NavParams) {
      plt.ready().then((readySource) => {
        this.nativeAudio.preloadSimple("do", "assets/DO.mp3").then(this.onSuccess, this.onError);
@@ -25,45 +28,77 @@ export class HomePage {
        this.usuarioNombre=params.data;
        console.info(params.data);
        this.archivo=file;
-
-       //this.archivo.createDir('./', 'txt', true);
-     this.archivo.checkDir(this.archivo.externalDataDirectory, '').then(_ => console.log('Directory exists')).catch(err => console.log('Directory doesnt exist'));
-      this.archivo.createFile(this.archivo.externalDataDirectory,"archivo.txt",true).then(_ => console.log('file creado')).catch(err => console.log('error'));
-      console.info(this.archivo.externalDataDirectory);
+       this.borrar();
+        console.info(this.archivo.externalDataDirectory);
   });
     
   }
 
   playDO(){ 
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"DO.mp3", "comandoEjec":"this.nativeAudio.play('do').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("do").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
-  }
+    this.todo.push(this.aguardar);
+    //this.archivo.writeFile(this.archivo.externalDataDirectory,"piano.txt", "this.aguardar", true).then(_ => console.log('ok')).catch(err => console.log('err'));
+ }
     playRE(){
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"RE.mp3", "comandoEjec":"this.nativeAudio.play('re').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("re").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+    this.todo.push(this.aguardar);
   }
     playMI(){
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"MI.mp3", "comandoEjec":"this.nativeAudio.play('mi').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("mi").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+    this.todo.push(this.aguardar);
   }
     playFA(){
+      
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"FA.mp3", "comandoEjec":"this.nativeAudio.play('fa').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("fa").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+      this.todo.push(this.aguardar);
   }l
     playSOL(){
+    
+    var unafecha = Date();
+    var aguardar= {"fecha":unafecha.toString(),"reproduccion":"SOL.mp3", "comandoEjec":"this.nativeAudio.play('sol').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("sol").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+     this.todo.push(this.aguardar);
   }
     playLA(){
+    
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"LA.mp3", "comandoEjec":"this.nativeAudio.play('la').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("la").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+     this.todo.push(this.aguardar);
   }
     playSI(){
+     
+    var unafecha = Date();
+    this.aguardar= {"fecha":unafecha.toString(),"reproduccion":"SI.mp3", "comandoEjec":"this.nativeAudio.play('si').then(this.onSuccess, this.onError)"};
     this.nativeAudio.play("si").then(this.onSuccess, this.onError);
     this.vibration.vibrate(100);
+     this.todo.push(this.aguardar);
   }
+ 
 
-
+    Salir()
+    {
+      this.archivo.writeFile(this.archivo.externalDataDirectory,"piano.txt", this.aguardar, true).then(_ => console.log('ok')).catch(err => console.log('errpiano'));
+      this.archivo.writeFile(this.archivo.externalDataDirectory,"todo.txt", this.todo, true).then(_ => console.log('ok')).catch(err => console.log('errtodo'));
+      console.info("aguardar: "+ this.aguardar);
+      console.info("todo: "+ this.todo);
+      alert("El archivo 'piano.txt' y 'todo.txt' se guardo en: "+ this.archivo.externalDataDirectory);
+      this.navCtrl.push(LoginPage);
+    } 
   onSuccess(){
     console.info("Ok al reproducir el sonido");
   }
@@ -71,6 +106,12 @@ export class HomePage {
     onError(){
     console.info("Error al reproducir el sonido");
   }
+
+
+   borrar(){
+           this.archivo.removeFile(this.archivo.externalDataDirectory,"piano.txt").then(_ => console.log('borrado ok')).catch(err => console.log('Archivo no encontrdo'));
+           this.archivo.removeFile(this.archivo.externalDataDirectory,"todo.txt").then(_ => console.log('borrado ok')).catch(err => console.log('Archivo no encontrdo'));
+      }
 
   
 }
